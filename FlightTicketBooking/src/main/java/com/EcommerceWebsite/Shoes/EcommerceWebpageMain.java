c0ba8af6c16a7652d2934d39209bc51e6e1f073d
godson.joseph <godson.joseph@mpokket.com>
2023-07-25 21:37:00 +0530

    Ecommerce Website

---
+++ FlightTicketBooking/src/main/java/com/EcommerceWebsite/Shoes/EcommerceWebpageMain.java
@@ -0,0 +1,13 @@
+package com.EcommerceWebsite.Shoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebpageMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebpageMain.class, args);
+	}
+
+}
